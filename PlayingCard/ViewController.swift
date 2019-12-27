72877a4c39f653ac41b9ccfeea3555f700f91c8e
JCNextCode <jae.jc.cho@gmail.com>
2019-12-27 17:17:46 -0500

    Lesson 5

--- PlayingCard/ViewController.swift
+++ PlayingCard/ViewController.swift
@@ -13,6 +13,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        var deck = PlayingCardDeck()
+        for _ in 1...10{
+            if let card = deck.draw(){
+                print("\(card)")
+            }
+        }
+        
     }
 
 
